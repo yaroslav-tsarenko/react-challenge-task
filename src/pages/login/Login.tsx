4619ebaf887e2d29a37f85bcf381e5e25f31a576
Yaroslav <yaroslav7v@gmail.com>
2024-09-12 16:14:42 +0300

    #RTT-005: implemented login page and home page

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '../../features/auth/components/LoginForm';
+
+export const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
