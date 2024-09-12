b0d61dc5cbb126178bf676b427b57efd7be77ec8
Yaroslav <yaroslav7v@gmail.com>
2024-09-13 00:32:36 +0300

    #RTT-006: routes implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+    createRootRoute,
+    createRoute,
+    createRouter, Outlet,
+    RouterProvider,
+} from "@tanstack/react-router";
+import { Login } from "./pages/login/Login";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootRoute = createRootRoute({
+    component: () => (
+        <div>
+            <h1>My App</h1>
+            <Outlet />
+        </div>
+    ),
+});
+
+const homeRoute = createRoute({
+    getParentRoute: () => rootRoute, // Define the parent
+    path: "/",
+    component: () => (
+        <div>
+            <h1>My App</h1>
+            <Outlet />
+        </div>
+    ),
+});
+
+const loginRoute = createRoute({
+    getParentRoute: () => rootRoute, // Define the parent
+    path: "/login",
+    component: Login,
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, loginRoute]);
+
+const router = createRouter({
+    routeTree,
+});
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
