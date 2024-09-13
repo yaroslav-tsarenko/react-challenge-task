4981b90131d4f7c3e655c7b7443c3ac89b57fb2b
Yaroslav <yaroslav7v@gmail.com>
2024-09-13 22:20:36 +0300

    #RTT-001: conf and dependencies changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
