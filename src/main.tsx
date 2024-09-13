6b9b4e68635022c13f4d4c2db4abe5b30ba9f709
Yaroslav <yaroslav7v@gmail.com>
2024-09-13 16:44:13 +0300

    #RTT-006: routes setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,10 @@
+import "reflect-metadata";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-    createRootRoute,
-    createRoute,
-    createRouter, Outlet,
-    RouterProvider,
-} from "@tanstack/react-router";
-import { Login } from "./pages/login/Login";
-
-const rootRoute = createRootRoute({
-    component: () => (
-        <div>
-            <h1>My App</h1>
-            <Outlet />
-        </div>
-    ),
-});
-
-const homeRoute = createRoute({
-    getParentRoute: () => rootRoute, // Define the parent
-    path: "/",
-    component: () => (
-        <div>
-            <h1>My App</h1>
-            <Outlet />
-        </div>
-    ),
-});
-
-const loginRoute = createRoute({
-    getParentRoute: () => rootRoute, // Define the parent
-    path: "/login",
-    component: Login,
-});
-
-const routeTree = rootRoute.addChildren([homeRoute, loginRoute]);
-
-const router = createRouter({
-    routeTree,
-});
+import { Routes } from "../src/routes/routes";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <Routes />
     </StrictMode>
 );
